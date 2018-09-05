d73885e7e1ee104778a8d30992a4f3dfc73565a9
Janak <janak@honeycombsoftwares.com>
2018-09-05 18:25:25 +0530

    First commit for testing

---
+++ WebApplication14/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication14.Startup))]
+namespace WebApplication14
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
